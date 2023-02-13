aa2ebfa3dce3bec16fe356b730567e9c6b16b93a
Tolga BEYLIK <tolga.beylik@viacesi.fr>
2023-02-13 09:13:07 +0100

    Premier commit

---
+++ EasySave v1.0.0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave_v1._0._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ EasySave v1.0.0/WorkSave.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave_v1._0._0
+{
+    abstract class WorkSave
+    {
+
+        public string name { get; set; }
+        public string name { get; set; }
+        public string name { get; set; }
+        public string name { get; set; }
+    }
+}
