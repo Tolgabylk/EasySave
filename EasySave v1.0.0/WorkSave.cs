41fbef0caf020e06142c611c889a54cc35ab995d
Tolga BEYLIK <tolga.beylik@viacesi.fr>
2023-02-13 09:17:11 +0100

    Ajout de la classe abstraite WorkSave

--- EasySave v1.0.0/WorkSave.cs
+++ EasySave v1.0.0/WorkSave.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace EasySave_v1._0._0
-{
+{/// <summary>
+/// Abtract calss for the complete and the differential Save
+/// </summary>
     abstract class WorkSave
     {
+        #region Properties
+        public string name { get; set; }
+        public string baseDirectory { get; set; }
+        public string targetDirectory { get; set; }
+        public bool type { get; set; }
 
-        public string name { get; set; }
-        public string name { get; set; }
-        public string name { get; set; }
-        public string name { get; set; }
+        #endregion Properties 
+
+        #region Constructor
+
+        public WorkSave(string Name, string BaseDirectory, string TargetDirectory, bool Type)
+        {
+            this.name = Name;
+            this.baseDirectory = BaseDirectory;
+            this.targetDirectory = TargetDirectory;
+            this.type = Type;
+        }
+
+        public override string ToString()
+        {
+            return this.name + " " + this.baseDirectory;
+        }
+
+        #endregion Constructor 
     }
 }
